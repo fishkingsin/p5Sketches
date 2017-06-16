6b16e7f3dc526503e29d3f2628c9f102e3f9110e
James Kong <james.kong@nixplay.com>
2017-06-16 09:05:36 +0800

    added sample

---
+++ sample1/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(0);
+  var step = 100;
+  
+  for (var j = 0; j < height; j += step) {
+    for (var i = 0; i < width; i += step) {
+      var tx = i+step*0.5;
+      var ty = j+step*0.5;
+      var radius = sin((millis()*0.0005)*PI*(i/width+j/height))*step;
+      ellipse(tx, ty, radius, radius);
+    }
+  }
+}
